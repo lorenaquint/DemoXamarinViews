c311087f30ed401dc78730e6ff8c1ac66eb116bf
lorenaquint <lorenaquint@gmail.com>
2018-04-02 23:56:53 -0500

    Priemor Views adicionando botn activity Indicator

---
+++ DemoXamarinViews/DemoXamarinViews/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DemoXamarinViews
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+        private void Button_Clicked(object sender, EventArgs args)
+
+        {
+            DisplayAlert("Mensaje", "Haz hecho clic", "Ok");
+        }
+    }
+}
