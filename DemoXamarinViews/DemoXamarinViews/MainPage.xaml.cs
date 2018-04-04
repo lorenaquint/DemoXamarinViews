d3a6af4d690cba3ad17ce7124c33541eaf64241e
lorenaquint <lorenaquint@gmail.com>
2018-04-04 11:26:36 -0500

    nuevos controles

--- DemoXamarinViews/DemoXamarinViews/MainPage.xaml.cs
+++ DemoXamarinViews/DemoXamarinViews/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,11 +13,30 @@ namespace DemoXamarinViews
 		public MainPage()
 		{
 			InitializeComponent();
-		}
-        private void Button_Clicked(object sender, EventArgs args)
+            //btnImg.Clicked += btnImg_OnClicked;
+            //dtp.MinimumDate = new DateTime(1990, 1, 1);
+            //dtp.MaximumDate = new DateTime(2020, 1, 1);
+            //dtp.DateSelected += Dtp_DateSelected;
 
-        {
-            DisplayAlert("Mensaje", "Haz hecho clic", "Ok");
         }
+
+        //private void Dtp_DateSelected(object sender, DateChangedEventArgs e)
+        //{
+        //    CultureInfo culture = new CultureInfo("es-ES");
+           
+        //   //DisplayAlert("Fecha", e.NewDate.ToString("D",culture), "Aceptar");
+        //    DisplayAlert("Fecha", e.NewDate.ToString(), "Aceptar");
+        //}
+
+        //private void btnImg_OnClicked(object sender, EventArgs e)
+        //{
+        //    DisplayAlert("Mensaje", "Manenando eventos con sintaxis estándar", "Aceptar");
+        //}
+
+        //private void btnImg_Clicked(object sender, EventArgs args)
+
+        //{
+        //    DisplayAlert("Mensaje", "Manejando eventos desde XAML", "Aceptar");
+        //}
     }
 }
